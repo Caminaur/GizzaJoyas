1629563b480db395b60acd2d6eaa32ebf5e7a032
tatenalan <taten.usa@gmail.com>
2020-08-17 04:25:29 -0300

    navbar mobile, vista profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -17,26 +17,24 @@ Perfil
       <br>
 
 
-        <table class="uk-table uk-table-small uk-table-divider">
-    <thead>
-        <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th class="uk-width-small"></th>
-            <th class="uk-width-small"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>{{ Auth::user()->name }}</td>
-            <td>{{ Auth::user()->email }}</td>
-            <td><a class="btn border-ashBlue" href="/editar-perfil">Editar Perfil</a></td>
-            <td><a class="btn border-dandelion" href="#confirm" uk-toggle>Eliminar Perfil</a></td>
+        <table class="uk-table uk-table-middle uk-table-divider">
+          <thead>
+              <tr>
+                  <th>Nombre</th>
+                  <th>Correo</th>                  
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>{{ Auth::user()->name }}</td>
+                  <td>{{ Auth::user()->email }}</td>
+              </tr>
+          </tbody>
+        </table>
 
-            @include('partials.confirm',['url'=>'/borrar-perfil', 'message'=>'Seguro quiere eliminar el usuario?'])
-        </tr>
-    </tbody>
-</table>
+        <a class="btn border-ashBlue" href="/editar-perfil">Editar</a>
+        <a class="btn border-dandelion" href="#confirm" uk-toggle>Eliminar</a>
+        @include('partials.confirm',['url'=>'/borrar-perfil', 'message'=>'Seguro quiere eliminar el usuario?'])
 
     </div>
 
